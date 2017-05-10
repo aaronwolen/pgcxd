7d26977a811702201622d4f25c9d58a0ac28772c
Aaron Wolen <aaron@wolen.com>
2017-05-10 08:57:36 -0400

    Add package documentation

---
+++ R/pgcxd.r
@@ -0,0 +1,18 @@
+#' PGC Cross Disorder Summary Statistics
+#'
+#' Summary statistics for the 5 individual disorders used in the Psychiatric
+#' Genomics Consortium (PGC) cross disorder analysis, including autism spectrum
+#' disorder (\code{aut}), attention deficit-hyperactivity disorder
+#' (\code{adhd}), bipolar disorder (\code{bip}), major depressive disorder
+#' (\code{mdd}), and schizophrenia (\code{scz}).
+#'
+#' @source https://www.med.unc.edu/pgc
+#'
+#' @references
+#' Cross-disorder group of the Psychiatric Genomics Consortium,
+#' Identification of risk loci with shared effects on five major psychiatric
+#' disorders: a genome-wide analysis. Lancet, epub (2013).
+#'
+#' @docType package
+#' @name pgcxd
+NULL
